f423baed3b34f29f2848b1b47e616b4ed1f912e5
MQYASEEN <qasimyaseen2@gmail.com>
2024-03-17 23:31:31 +0500

    words list updated

--- hangman.py
+++ hangman.py
@@ -3,19 +3,30 @@ import hangman_words
 import hangman_art
 print(hangman_art.logo)
 display=[]
+game_end=False
+lives=6
 choosen_word=random.choice(hangman_words.words_list)
 print(f'You Choosen Solution is : {choosen_word}')
 for _ in range(len(choosen_word)):
     display+="_"
-print(display)
-game_end=False
 while not game_end:
     guess_word=input("Guess a Letter:").lower()
+    if guess_word in display:
+        print(f'The Letter {guess_word} Already Selected')
     for location in range(len(choosen_word)):
         letter=choosen_word[location]
         if letter==guess_word:
             display[location]=letter
             print(display)
-if "_" not in display:
-    game_end=True
-    print("You Win")
+
+    if guess_word not in choosen_word:
+        print(f'Your Guess Letter Not Match! You Loose a life')
+        lives-=1
+        print(hangman_art.stages[lives])    
+
+    if lives==0:
+        game_end=True
+        print("You Loose")
+    if "_" not in display:
+        game_end=True
+        print("You Win")
--- hangman_words.py
+++ hangman_words.py
@@ -1,1 +1,10 @@
-words_list=['hamza','qasim','ali','shahbaz','mohsin']
+words_list = ['apple', 'banana', 'orange', 'grape', 'strawberry', 'kiwi', 'pineapple', 'mango', 'watermelon', 'peach',
+          'pear', 'cherry', 'blueberry', 'raspberry', 'lemon', 'lime', 'plum', 'apricot', 'fig', 'pomegranate',
+          'nectarine', 'coconut', 'avocado', 'guava', 'passion fruit', 'dragon fruit', 'lychee', 'persimmon', 'papaya',
+          'dog', 'cat', 'horse', 'rabbit', 'hamster', 'guinea pig', 'ferret', 'gerbil', 'mouse', 'rat',
+          'parrot', 'canary', 'finch', 'budgerigar', 'cockatiel', 'lovebird', 'macaw', 'cockatoo', 'african grey', 'amazon',
+          'goldfish', 'betta fish', 'guppy', 'koi', 'tetra', 'swordtail', 'molly', 'platy', 'angel fish', 'discus',
+          'turtle', 'tortoise', 'gecko', 'iguana', 'chameleon', 'skink', 'anole', 'bearded dragon', 'snake', 'python',
+          'frog', 'toad', 'newt', 'salamander', 'axolotl', 'tadpole', 'hermit crab', 'scorpion', 'tarantula', 'millipede',
+          'centipede', 'beetle', 'ant', 'grasshopper', 'cricket', 'caterpillar', 'butterfly', 'moth', 'bee', 'wasp',
+          'spider', 'fly', 'mosquito', 'ladybug', 'firefly', 'dragonfly', 'beetle', 'crab', 'lobster', 'shrimp', 'prawn']
